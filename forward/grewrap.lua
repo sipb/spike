8ba118074309f827290904a30c35a62b4c2fe47d
ikdc <ikdc@mit.edu>
2017-06-28 01:30:48 -0400

    Basic GRE encapsulation script

---
+++ forward/grewrap.lua
@@ -0,0 +1,37 @@
+local P = require("core.packet")
+local L = require("core.link")
+local G = require("lib.protocol.gre")
+local D = require("lib.protocol.datagram")
+
+local M = {}
+
+_G._NAME = "gre_wrap"  -- Snabb requires this for some reason
+
+
+GRE_wrap = {}
+
+function GRE_wrap:new(protocol)
+   return setmetatable({protocol = protocol}, {__index = GRE_wrap})
+end
+
+function GRE_wrap:push()
+   local i = assert(self.input.input, "input port not found")
+   local o = assert(self.output.output, "output port not found")
+   while not L.empty(i) do
+      self:process_packet(i, o)
+   end
+end
+
+function GRE_wrap:process_packet(i, o)
+   local p = L.receive(i)
+   local gre_header = G:new({protocol = self.protocol})
+   local datagram = D:new(p, self.protocol, {delayed_commit = false})
+   datagram:push(gre_header)
+
+   link.transmit(o, datagram:packet())
+end
+
+M.GRE_wrap = GRE_wrap
+
+
+return M
---
+++ forward/spike.lua
@@ -0,0 +1,19 @@
+local B = require("apps.basic.basic_apps")
+local P = require("apps.pcap.pcap")
+local G = require("grewrap")
+
+local c = config.new()
+
+ETHERNET = 0x6558
+IPV4 = 0x0800
+IPV6 = 0x86DD
+
+config.app(c, "source", B.Source, 60)
+config.app(c, "gre", G.GRE_wrap, IPV6)
+config.app(c, "sink", P.PcapWriter, "out.pcap")
+config.link(c, "source.tx -> gre.input")
+config.link(c, "gre.output -> sink.input")
+-- config.link(c, "source.tx -> sink.input")
+
+engine.configure(c)
+engine.main({duration = 1, report = {showlinks = true}})
