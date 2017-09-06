43d62badb02f280be98fee6a0c9a5a4f5103b732
twang6 <twang6@mit.edu>
2017-09-05 23:52:53 -0400

    Add basic functionality test

--- maglev/maglev_test.go
+++ maglev/maglev_test.go
@@ -1,8 +1,11 @@
 package maglev
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
+	"github.com/sipb/spike/common"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -13,4 +16,38 @@ func TestTableSize(t *testing.T) {
 	New(BigM)
 	assert.Panics(t, func() { New(1 << 60) }, "2^60 is not prime but table created")
 	assert.Panics(t, func() { New(57) }, "57 is not prime but table created")
+}
+
+func TestBasicFunctionality(t *testing.T) {
+	table := New(SmallM)
+
+	backends := make([]common.Backend, 6)
+	for i := 0; i < len(backends); i++ {
+		backends[i] = common.Backend{IP: []byte{0, 0, 0, byte(i)}}
+	}
+
+	table.Add(&backends[0])
+	table.Add(&backends[1])
+	table.Add(&backends[2])
+
+	table.SetWeight(&backends[3], 2)
+	table.SetWeight(&backends[3], 3)
+
+	table.Add(&backends[4])
+	table.Remove(&backends[4])
+
+	table.Add(&backends[5])
+	table.SetWeight(&backends[5], 0)
+
+	rand.Seed(42)
+	freq := make(map[*common.Backend]uint)
+	for i := 0; i < 1e4; i++ {
+		cur, _ := table.Lookup(rand.Uint64())
+		freq[cur] = freq[cur] + 1
+	}
+
+	assert.Equal(t, 4, len(freq), "There should only be three backends.")
+	for i := 0; i < 4; i++ {
+		assert.True(t, freq[&backends[i]] > 0, fmt.Sprintf("backends[%d] not hit", i))
+	}
 }
