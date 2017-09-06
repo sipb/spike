4cf17a7ff82393c76967ad09c108e81c683785bc
twang6 <twang6@mit.edu>
2017-09-06 00:03:15 -0400

    Rename test, Add TestReconfig

--- maglev/maglev_test.go
+++ maglev/maglev_test.go
@@ -18,13 +18,13 @@ func TestTableSize(t *testing.T) {
 	assert.Panics(t, func() { New(57) }, "57 is not prime but table created")
 }
 
-func TestBasicFunctionality(t *testing.T) {
-	table := New(SmallM)
-
+func TestAddAndRemove(t *testing.T) {
 	backends := make([]common.Backend, 6)
 	for i := 0; i < len(backends); i++ {
 		backends[i] = common.Backend{IP: []byte{0, 0, 0, byte(i)}}
 	}
+
+	table := New(SmallM)
 
 	table.Add(&backends[0])
 	table.Add(&backends[1])
@@ -48,6 +48,33 @@ func TestBasicFunctionality(t *testing.T) {
 
 	assert.Equal(t, 4, len(freq), "There should only be three backends.")
 	for i := 0; i < 4; i++ {
+		assert.True(t, freq[&backends[i]] > 0, fmt.Sprintf("backends[%d] not hit", i))
+	}
+}
+
+func TestReconfig(t *testing.T) {
+	backends := make([]common.Backend, 4)
+	for i := 0; i < len(backends); i++ {
+		backends[i] = common.Backend{IP: []byte{0, 0, 0, byte(i)}}
+	}
+
+	config := make(Config)
+	for i := 0; i < len(backends); i++ {
+		config[&backends[i]] = uint(i)
+	}
+
+	table := New(SmallM)
+	table.Reconfig(config)
+
+	rand.Seed(42)
+	freq := make(map[*common.Backend]uint)
+	for i := 0; i < 1e4; i++ {
+		cur, _ := table.Lookup(rand.Uint64())
+		freq[cur] = freq[cur] + 1
+	}
+
+	assert.Equal(t, len(backends)-1, len(freq), fmt.Sprintf("There should be %d backends.", len(backends)))
+	for i := 1; i < 4; i++ {
 		assert.True(t, freq[&backends[i]] > 0, fmt.Sprintf("backends[%d] not hit", i))
 	}
 }
