4c38e04263744980c01be395c81a809498446246
krawthekrow <krawthekrow@gmail.com>
2017-09-30 16:08:00 -0400

    make packet synthesis more modular; compute correct tcp checksum

--- forward/testing/forward_test.lua
+++ forward/testing/forward_test.lua
@@ -11,7 +11,7 @@ local packet = require("core.packet")
 local Rewriting = require("rewriting")
 local godefs = require("godefs")
 
-local packet_synthesis = require("testing/packet_synthesis")
+local PacketSynthesisContext = require("testing/packet_synthesis")
 local TestStreamApp = require("testing/test_stream_app")
 
 local function runmain()
@@ -22,36 +22,28 @@ local function runmain()
                      IPV4:pton("2.4.6.8"), 4)
    C.usleep(3000000) -- wait for backends to come up
 
-   local spike_mac = "38:c3:0d:1d:34:df"
-   local router_mac = "ce:d2:85:61:1e:01"
-   local backend_vip_addr = "18.0.0.0"
-   local router_addr = "18.100.100.100"
-   local client_addr = "1.0.0.0"
-   local spike_internal_addr = "192.168.1.0"
-   local other_spike_internal_addr = "192.168.1.1"
+   local network_config = {
+      spike_mac = "38:c3:0d:1d:34:df",
+      router_mac = "ce:d2:85:61:1e:01",
+      backend_vip_addr = "18.0.0.0",
+      client_addr = "1.0.0.0",
+      spike_internal_addr = "192.168.1.0",
+      other_spike_internal_addr = "192.168.1.1",
+      backend_vip_port = 80,
+      client_port = 12345
+   }
+
+   local synthesis = PacketSynthesisContext:new(network_config)
 
    local test_fragmentation = true
    local debug_bypass_spike = false
 
    local packets
    if test_fragmentation then
-      packets = make_fragmented_ipv4_packets({
-         src_mac = Ethernet:pton(router_mac),
-         dst_mac = Ethernet:pton(spike_mac),
-         src_addr = IPV4:pton(client_addr),
-         dst_addr = IPV4:pton(backend_vip_addr),
-         outer_src_addr = IPV4:pton(other_spike_internal_addr),
-         outer_dst_addr = IPV4:pton(spike_internal_addr),
-         add_ip_gre_layer = true
-      })
+      packets = synthesis:make_redirected_ipv4_fragment_packets()
    else
       packets = {
-         [1] = make_ipv4_packet({
-            src_mac = Ethernet:pton(router_mac),
-            dst_mac = Ethernet:pton(spike_mac),
-            src_addr = IPV4:pton(client_addr),
-            dst_addr = IPV4:pton(backend_vip_addr)
-         })
+         [1] = synthesis:make_ipv4_packet()
       }
    end
 
@@ -60,9 +52,9 @@ local function runmain()
       packets = packets
    })
    config.app(c, "spike", Rewriting, {
-      src_mac = spike_mac,
-      dst_mac = router_mac,
-      ipv4_addr = spike_internal_addr
+      src_mac = network_config.spike_mac,
+      dst_mac = network_config.router_mac,
+      ipv4_addr = network_config.spike_internal_addr
    })
    config.app(c, "pcap_writer", P.PcapWriter, "test_out.pcap")
    if debug_bypass_spike then
